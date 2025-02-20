4557caf438916976320bd42372235bd65da913aa
LBWGitHub001 <13032652089@163.com>
2025-02-20 21:27:57 +0800

    [FEAT]模版实现AsyncInferer

--- include/inferer/AsyncInferer.h
+++ include/inferer/AsyncInferer.h
@@ -26,25 +26,120 @@ typedef struct
     long timestamp;
 } Task;
 
+template<class _Infer>
 class AsyncInferer
 {
-
 public:
-    explicit AsyncInferer();
-    ~AsyncInferer();
+    explicit AsyncInferer()
+    {
+        result_start_ = true;
+        std::thread th(&AsyncInferer::result_loop, this);
+        th.detach();
 
-    bool setInfer(std::unique_ptr<InferBase> infer);
-    void registerPostprocess(std::function<void*(std::vector<void*>&, std::vector<det::Binding>&)> callback);
-    void registerCallback(std::function<void(void*)> callback);
+    }
+    ~AsyncInferer()
+    {
+        thread_pool_.join();
+        result_start_ = false;
+        std::this_thread::sleep_for(std::chrono::milliseconds(result_delay_*5));
+    }
 
-    void pushInput(const std::function<void(void**, std::vector<det::Binding>&)>& input_bindings);
+    bool setInfer(std::unique_ptr<InferBase> infer)
+    {
+        try
+        {
+            infer_ = std::move(infer);
+            getNetStructure();
+        }
+        catch (...)
+        {
+            return false;
+        }
+        return true;
+    }
+    void registerPostprocess(std::function<void*(std::vector<void*>&, std::vector<det::Binding>&)> callback)
+    {
+        post_function_ = std::move(callback);
+        std::cout << "Registering callback function" << std::endl;
+    }
+    void registerCallback(std::function<void(void*)> callback)
+    {
+        callback_ = std::move(callback);
+    }
 
-    void set_getResult_timer(int time);
+    void pushInput(const std::function<void(void**, std::vector<det::Binding>&)>& get_input)
+    {
+        auto ff = [this,get_input](int pool_id, int thread_id)
+        {
+            void* input = nullptr;
+            get_input(&input, input_bindings_);
+            if (ThreadPool::try_to_malloc_static(pool_id, thread_id, infer_->get_size()))
+            {
+                void* ptr;
+                get_staticMem_ptr(pool_id, thread_id, &ptr);
+                auto name = infer_->get_name();
+                if (name == "VinoInfer")
+                {
+                    auto* infer = static_cast<VinoInfer*>(ptr);
+                    // infer->VinoInfer(infer_->getModelPath(),false,dynamic_cast<VinoInfer*>(infer_.get())->getDevice());
+                    infer->setModel(infer_->getModelPath());
+                    infer->setDevice(dynamic_cast<VinoInfer*>(infer_.get())->getDevice());
+                    infer->init();
+                }
+                else if (name == "TrtInfer")
+                {
+                    auto* infer = static_cast<TrtInfer*>(ptr);
+                    infer->setModel(infer_->getModelPath());
+                    infer->init();
+                }
+                else
+                {
+                    throw "Unknown Infer";
+                }
+            }
+            void* ptr;
+            get_staticMem_ptr(pool_id, thread_id, &ptr);
+            std::vector<void*> output_vec;
+            auto name = infer_->get_name();
+            if (name == "VinoInfer")
+            {
+                auto* infer = static_cast<VinoInfer*>(ptr);
+                infer->copy_from_data(input);
+                infer->infer();
+            }
+            else if (name == "TrtInfer")
+            {
+                auto* infer = static_cast<TrtInfer*>(ptr);
+                infer->copy_from_data(input);
+                infer->infer();
+            }
+            else
+            {
+                throw "Unknown Infer";
+            }
+            // auto output_vec = infer->getResult();
+            post_function_(output_vec, output_bindings_);
+            return nullptr;
+        };
+
+        thread_pool_.push(std::move(ff));
+    }
+    void set_getResult_timer(int time)
+    {
+        result_delay_ = time;
+    }
 private:
-    void getNetStructure();
+    void getNetStructure()
+    {
+        input_bindings_ = infer_->getInputBinding();
+        output_bindings_ = infer_->getOutputBinding();
+    }
+
     //网络信息
     std::vector<det::Binding> input_bindings_;
     std::vector<det::Binding> output_bindings_;
+    std::string model_path_;
+    std::string device_path;
     //推理器管理
     std::shared_ptr<InferBase> infer_;
     std::function<void*(std::vector<void*>&, std::vector<det::Binding>&)> post_function_;
@@ -54,34 +149,22 @@ private:
     int result_delay_ = 100;
     bool result_start_ = true;
     std::function<void(void*)> callback_;
-    void result_loop();
-};
-
-
-inline void AsyncInferer::set_getResult_timer(int time)
-{
-    result_delay_ = time;
-}
-
-inline void AsyncInferer::registerCallback(std::function<void(void*)> callback)
-{
-    callback_ = std::move(callback);
-}
-
-inline void AsyncInferer::result_loop()
-{
-    while (result_start_)
+    void result_loop()
     {
-        void* output;
-        if (thread_pool_.fast_get(&output))
+        while (result_start_)
         {
-            callback_(output);
-            free(output);
-        }
-        else
-        {
-            std::this_thread::sleep_for(std::chrono::milliseconds(result_delay_));
+            void* output;
+            if (thread_pool_.fast_get(&output))
+            {
+                callback_(output);
+                free(output);
+            }
+            else
+            {
+                std::this_thread::sleep_for(std::chrono::milliseconds(result_delay_));
+            }
         }
     }
-}
+};
+
 #endif //ASYNCINFERER_H
--- src/test.cpp
+++ src/test.cpp
@@ -6,8 +6,9 @@
 #include <thread>
 using namespace std::placeholders;
 
-void preProcess(void** input, std::vector<det::Binding>& input_bindings, cv::Mat img)
+void preProcess(void** input, std::vector<det::Binding>& input_bindings, cv::Mat& img)
 {
+    std::cout << "preProcess Start" << std::endl;
     cv::Mat img_tmp;
     auto input_binding = input_bindings[0];
     auto input_dim = input_binding.dims.d;
@@ -21,7 +22,7 @@ void preProcess(void** input, std::vector<det::Binding>& input_bindings, cv::Mat
     {
         img_tmp.convertTo(img_tmp, cv_type, 1 / 255.0);
     }
-
+    std::cout << "Preprocess Done" << std::endl;
     cv::Mat input_tensor = cv::dnn::blobFromImage(img_tmp);
     auto out = input_tensor.ptr<float>();
     int size = input_bindings[0].size * input_bindings[0].dsize;
@@ -29,10 +30,11 @@ void preProcess(void** input, std::vector<det::Binding>& input_bindings, cv::Mat
     memcpy(*input, out, size);
 }
 
-void postProcess(void* output, std::vector<det::Binding>& output_bindings,long timestamp)
+void* postProcess(std::vector<void*>& output_vec, std::vector<det::Binding>& output_bindings)
 {
-    std::vector<cv::Rect> results;
-    // std::cout << "postprocessing" << std::endl;
+    auto output = output_vec[0];
+    std::vector<cv::Rect>* results = (std::vector<cv::Rect>*)malloc(sizeof(std::vector<cv::Rect>));
+    std::cout << "postprocessing" << std::endl;
     std::vector<cv::Rect> rects;
     std::vector<float> confs;
     const det::Binding& data = output_bindings[0];
@@ -59,50 +61,40 @@ void postProcess(void* output, std::vector<det::Binding>& output_bindings,long t
     cv::dnn::NMSBoxes(rects, confs, 0.6, 0.6, indexes);
     for (auto i : indexes)
     {
-        results.push_back(rects[i]);
+        results->push_back(rects[i]);
     }
-    // std::cout << "PostProcess Done" << std::endl;
-    std::vector<float> tmp;
-    for (int i = 0; i < size; i++)
-    {
-        tmp.push_back(buf[i]);
-    }
-    tmp[0];
+    return results;
+}
+
+void callback(void* result)
+{
+    std::cout << "copy that" << std::endl;
 }
 
 int main()
 {
-    // AsyncInferer infer;
-    //
-    // infer.setInfer(std::make_unique<AUTO_INFER>(
-    //     "/home/lbw/RM2025/kalman-fix/AsyncInferFrame/model/best.onnx"
-    // ));
-    // infer.registerCallback(postProcess);
+    AsyncInferer infer;
+
+    infer.setInfer(std::make_unique<AUTO_INFER>(
+        "/home/lbw/RM2025/kalman-fix/AsyncInferFrame/model/robot.engine"
+    ));
+    infer.registerPostprocess(postProcess);
 
     std::string path = "/home/lbw/RM2025/kalman-fix/RM2024_nice/src/rm_utils/picture/0000";
 
     ThreadPool pool;
-    for (auto j=0;j<5;j++)
+    for (auto j = 0; j < 1; j++)
     {
-           for (auto i = 0; i < 30; i++)
-           {
-               std::string num = std::to_string(i);
-               if (i < 10)
-                   num = "0" + num;
-               std::string pic = num + ".jpg";
-               std::string pic_path = path + pic;
-               cv::Mat img = cv::imread(pic_path, cv::IMREAD_COLOR);
-               // infer.pushInput(std::bind(preProcess, _1, _2, std::move(img.clone())), i+j*30);
-               //        std::this_thread::sleep_for(std::chrono::microseconds(100));
-               // pool.push([&]()
-               // {
-               //     void* input = nullptr;
-               //
-               //     preProcess(input,)
-               //
-               //     return
-               // });
-    }
+        for (auto i = 0; i < 1; i++)
+        {
+            std::string num = std::to_string(i);
+            if (i < 10)
+                num = "0" + num;
+            std::string pic = num + ".jpg";
+            std::string pic_path = path + pic;
+            cv::Mat img = cv::imread(pic_path, cv::IMREAD_COLOR);
+            infer.pushInput(std::bind(preProcess, _1, _2, std::move(img.clone())));
+        }
     }
 
     return 0;
